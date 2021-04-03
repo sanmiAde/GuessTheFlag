6f97d4f624e4741e1bc360d92887fd1aa92d26bf
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-03 07:29:32 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by sanmi_personal on 03/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
