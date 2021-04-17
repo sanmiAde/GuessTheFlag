32384d833cb6937776a7760c80e7b671c01145f1
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-17 13:44:01 +0200

    add animation

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
     @State private var message = ""
+    @State private var selectedNumber = 0
+    @State private var correctFlag = false
+    @State private var wrongFlag = false
+    @State private var makeFlagOpaque = false
     
     var body: some View {
         ZStack {
@@ -25,21 +29,24 @@ struct ContentView: View {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
                     Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
+                        .modifier(LargwWhiteText())
+                    
+                    
                 }
                 
                 ForEach(0..<3 ) { number in
                     Button(action: {
-                        self.flagTapped(number)
+                        withAnimation {
+                            self.flagTapped(number)
+                        }
                     }){
                         Image(self.countries[number])
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
                             .shadow(color: .black, radius: 2)
-                    }
+                    }.rotation3DEffect(.degrees(self.correctFlag && self.selectedNumber == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
+                    .opacity(self.makeFlagOpaque && !(self.selectedNumber == number) ? 0.25 : 1)
                 }
                 Text("User score: \(userScore)").foregroundColor(.white)
                 Spacer()
@@ -54,21 +61,35 @@ struct ContentView: View {
     }
     
     func flagTapped(_ number: Int) {
+        selectedNumber = number
         if (number == correctAnswer) {
             scoreTitle = "Correct"
             userScore += 1
             message = "Your score is \(userScore)"
+            correctFlag = true
         } else {
             scoreTitle = "Wrong"
-            message = "Wrong! That’s the flag of \(countries[correctAnswer])"
-
+            message = "Wrong! That’s not the flag of \(countries[correctAnswer])"
+            userScore -= 1
+            wrongFlag = true
         }
         
         showingScore = true
     }
-       
+    
     func askQuestion() {
-        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        wrongFlag = false
+        correctFlag = false
+    }
+}
+
+struct LargwWhiteText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.largeTitle)
     }
 }
 
